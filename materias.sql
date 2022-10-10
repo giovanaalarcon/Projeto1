a0f76504cbe48ff0594a192405bff4cf065def9e
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2022-10-10 15:51:43 -0300

    Add files via upload

---
+++ materias.sql
@@ -0,0 +1,19 @@
+create table MateriasDB
+create table materias (
+    NOME varchar(100),
+    ID_MATERIA int,
+    SALA varchar(10)
+);
+insert into materias 
+values
+    ('Pi: Desenvolvimento de Sistemas Web','12902','H15_S04'),
+    ('Organizacao de Sistemas de Computacao','12903','H15_110'),
+    ('Calculo 1','11445','H15_111')
+    ('Calculo 2','11446','H15_120');
+    ('Robotica teorica','23415','CTA_09')
+    ('Robotica pratica','23489','H15_01')
+    ('Teologia','78965','H15_20')
+    ('Algebra linear','56787','CT_03')
+    ('Eletronica embarcada','45321','CT_04')
+    ('Vida universitaria','34162','H15_08')
+    ('Programacao em C','78905','H15_16')
