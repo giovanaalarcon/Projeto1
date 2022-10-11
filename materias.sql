569d6462bf8fa7baee7c7513f05f265ac51cc0ac
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2022-10-11 10:11:13 -0300

    Update materias.sql

--- materias.sql
+++ materias.sql
@@ -10,10 +10,9 @@ values
     ('Organizacao de Sistemas de Computacao','12903','H15_110'),
     ('Calculo 1','11445','H15_111'),
     ('Calculo 2','11446','H15_120'),
-    ('Robotica teorica','23415','CTA_09'),
-    ('Robotica pratica','23489','H15_01'),
+    ('Robotica Computacional','23415','CTA_09'),
     ('Teologia','78965','H15_20'),
-    ('Algebra linear','56787','CT_03'),
-    ('Eletronica embarcada','45321','CT_04'),
-    ('Vida universitaria','34162','H15_08'),
-    ('Programacao em C','78905','H15_16');
+    ('Algebra Linear','56787','CT_03'),
+    ('Eletronica Embarcada','45321','CT_04'),
+    ('Vida Universitaria','34162','H15_08'),
+    ('Programacao de Computadores','78905','H15_16');
