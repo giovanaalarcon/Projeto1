01f3eb43c95efeed49b9b2a2f59d18952d566673
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2022-10-11 09:56:38 -0300

    Update alunos.sql

--- alunos.sql
+++ alunos.sql
@@ -15,18 +15,16 @@ values
     ('Daniela Hayashi','22000002','daniela.hayashi@puccampinas.edu.br', 'Spirit'),
     ('Flavia Medeiros','22000003','flavia.medeiros@puccampinas.edu.br', 'High School Musical'),
     ('Giovana Alarcon','22000004','giovana.alarcon@puccampinas.edu.br', 'UP'),
-    ('Luana Baptista','22000005','luana.baptista@puccampinas.edu.br', 'Toy Story');
-
-    ('Gabriel Yamamoto','2200006','gabriel.yama@puccampinas.edu.br', 'Violeta');
-    ('Jose Martins','22000007','jose.martins@puccampinas.edu.br', 'After');
-    ('Rafael Luis','22000008','rafael.luis@puccampinas.edu.br', 'Garotas S.A');
-    ('Julia Silva','22000009','julia.silva@puccampinas.edu.br', 'Amigos para sempre');
-    ('Luis Gustavo','22000010','luis.gustavo@puccampinas.edu.br', 'Justiceir');
-
-    ('Joao Vitor','22000011','joao.vitor@puccampinas.edu.br', 'Sing');
-    ('Joao Pedro','22000012','joao.pedro@puccampinas.edu.br', 'As branquelas');
-    ('Luiza Souza','22000013','luiza.souza@puccampinas.edu.br', 'Emma');
-    ('Ananda Maia','22000014','ananda.maia@puccampinas.edu.br', 'Orgulho e Preconceito');
+    ('Luana Baptista','22000005','luana.baptista@puccampinas.edu.br', 'Toy Story'),
+    ('Gabriel Yamamoto','2200006','gabriel.yama@puccampinas.edu.br', 'Violeta'),
+    ('Jose Martins','22000007','jose.martins@puccampinas.edu.br', 'After'),
+    ('Rafael Luis','22000008','rafael.luis@puccampinas.edu.br', 'Garotas S.A'),
+    ('Julia Silva','22000009','julia.silva@puccampinas.edu.br', 'Amigos para sempre'),
+    ('Luis Gustavo','22000010','luis.gustavo@puccampinas.edu.br', 'Justiceir'),
+    ('Joao Vitor','22000011','joao.vitor@puccampinas.edu.br', 'Sing'),
+    ('Joao Pedro','22000012','joao.pedro@puccampinas.edu.br', 'As branquelas'),
+    ('Luiza Souza','22000013','luiza.souza@puccampinas.edu.br', 'Emma'),
+    ('Ananda Maia','22000014','ananda.maia@puccampinas.edu.br', 'Orgulho e Preconceito'),
     ('Julio Cesar','22000015','julio.cesar@puccampinas.edu.br', 'Plano B');
     
 
